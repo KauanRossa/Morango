58a8d010234c7713307fdd7bfa679e5d6c546af7
Kauan Rossa <125710216+KauanRossa@users.noreply.github.com>
2023-11-20 00:30:44 -0300

    Add files via upload

--- login_person.js
+++ login_person.js
@@ -8,56 +8,72 @@ document.addEventListener('DOMContentLoaded', function() {
     inputSenha.value = '';
   }
 
-  function realizarLogin() {
+  async function realizarLogin() {
     const dadosLogin = {
       email: inputEmail.value,
       senha: inputSenha.value
     };
 
-    fetch('https://33d6-189-28-184-55.ngrok-free.app/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(dadosLogin)
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
+    try {
+      const response = await fetch('https://33d6-189-28-184-55.ngrok-free.app/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dadosLogin)
+      });
+
+      if (!response.ok) {
         throw new Error('Credenciais inválidas. Verifique seu email e senha.');
       }
-    })
-    .then(data => {
+
+      const data = await response.json();
+
       // Armazena o token no localStorage
       localStorage.setItem('token', data.token);
       console.log('Login realizado:', data);
-      // Redireciona para a página index.html após o login
-      window.location.href = './index.html';
-      // Altera o ícone para logout
-      const cadastroLink = document.querySelector("#cadastroLink");
-      if (cadastroLink) {
-        cadastroLink.innerHTML = '<ion-icon name="log-out-outline"></ion-icon>';
-        cadastroLink.addEventListener('click', function(e) {
-          e.preventDefault();
-          // Remove o token do localStorage (simulando o logout)
-          localStorage.removeItem('token');
-          // Exibe o alerta informando que o usuário foi deslogado
-          alert('Você foi deslogado da sua conta.');
-          // Redireciona para a página de login
-          window.location.href = './login.html';
-        });
+
+      // Obter tipo de usuário
+      const tipoUsuario = await obterTipoUsuario();
+
+      // Redireciona para a página correspondente com base no tipo de usuário
+      if (tipoUsuario === 'ADMINISTRADOR') {
+        window.location.href = './index_admin.html';
+      } else if (tipoUsuario === 'CLIENTE') {
+        window.location.href = './index.html';
       }
-      // Limpa os campos do formulário
-      limparCampos();
-    })
-    .catch(error => {
+
+    } catch (error) {
       console.error('Erro ao realizar login:', error);
       // Exibe um alerta para o usuário quando as credenciais estiverem incorretas
       alert(error.message);
       // Limpa os campos do formulário
       limparCampos();
-    });
+    }
+  }
+
+  async function obterTipoUsuario() {
+    const token = localStorage.getItem('token');
+
+    try {
+      const response = await fetch('https://33d6-189-28-184-55.ngrok-free.app/api/usuarios/buscar-por-token', {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao obter as informações do usuário');
+      }
+
+      const usuarioLogado = await response.json();
+      return usuarioLogado.tipoUsuario;
+
+    } catch (error) {
+      console.error('Erro ao obter as informações do usuário:', error);
+      throw error;
+    }
   }
 
   formularioLogin.addEventListener('submit', function(event){
